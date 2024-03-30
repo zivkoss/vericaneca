55c79d332b1d7e8300cbbff576f36ca6c1197b47
Nemanja Zivanovic <zivkoss@yahoo.com>
2024-03-30 18:50:20 +0100

    add new py.code

--- projects.py
+++ projects.py
@@ -85,12 +85,22 @@
 
 # print_kevin()    
 
-def school_age_calculator(age,name):
-    if age < 5:
-       print("Enjoy the tim!", name, "is only", age)
-    elif age == 5:
-       print("Enjoy kindergarten,", name)
-    else:
-       print("They grow so fast")
 
-school_age_calculator(5,"Thomas")              
+# ==========================
+# def school_age_calculator(age,name):
+#     if age < 5:
+#        print("Enjoy the tim!", name, "is only", age)
+#     elif age == 5:
+#        print("Enjoy kindergarten,", name)
+#     else:
+#        print("They grow so fast")
+
+# school_age_calculator(5,"Thomas")    
+
+# =================
+def add_ten_to_age(age):
+    new_age = age + 10
+    return new_age
+
+How_Old_Will_I_Be = add_ten_to_age(3)
+print(How_Old_Will_I_Be)          
