a832e937abf9cec1cd4f54cabf05606b2fb5e69d
Nemanja Zivanovic <zivkoss@yahoo.com>
2024-03-30 18:53:43 +0100

    add new py.code

--- projects.py
+++ projects.py
@@ -98,9 +98,21 @@
 # school_age_calculator(5,"Thomas")    
 
 # =================
-def add_ten_to_age(age):
-    new_age = age + 10
-    return new_age
+# def add_ten_to_age(age):
+#     new_age = age + 10
+#     return new_age
 
-How_Old_Will_I_Be = add_ten_to_age(3)
-print(How_Old_Will_I_Be)          
+# How_Old_Will_I_Be = add_ten_to_age(3)
+# print(How_Old_Will_I_Be)     
+
+# ============
+
+# while     
+x=0
+while (x<5):
+    print(x)
+    x=x+1
+    
+# for
+    for x in range(5,10):
+        print(x)    
