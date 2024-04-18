f96914dd74c5a0c0c0542fdce35f37b3a93d661b
Nemanja Zivanovic <zivkoss@yahoo.com>
2024-04-19 00:18:16 +0200

    add/git commands

---
+++ git commands/git.r
@@ -0,0 +1,11 @@
+1593  git add .
+ 1594  git commit -m "any"
+ 1595  git push
+ 1596  git fetch origin
+ 1597  git reset --hard origin/master
+ 1598  git push -f origin main
+ 1599  git push -f origin master
+ 1600  clear
+ 1601  git add .
+ 1602  git commit -m "sdlk"
+ 1603  git push origin master
