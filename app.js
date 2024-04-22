6a7e7bc5f30a28d72dda36d8ba66a3a46d920bbe
Nemanja Zivanovic <zivkoss@yahoo.com>
2024-04-22 22:52:38 +0200

    add new lines

--- app.js
+++ app.js
@@ -65,41 +65,55 @@
 //  тада ће принети на жртвеник Твој теоце.${molitva}`)
 // 	}
 
-const contacts = [
-      {
-        firstName: "Akira",
-        lastName: "Laine",
-        number: "0543236543",
-        likes: ["Pizza", "Coding", "Brownie Points"],
-      },
-      {
-        firstName: "Harry",
-        lastName: "Potter",
-        number: "0994372684",
-        likes: ["Hogwarts", "Magic", "Hagrid"],
-      },
-      {
-        firstName: "Sherlock",
-        lastName: "Holmes",
-        number: "0487345643",
-        likes: ["Intriguing Cases", "Violin"],
-      },
-      {
-        firstName: "Kristian",
-        lastName: "Vos",
-        number: "unknown",
-        likes: ["JavaScript", "Gaming", "Foxes"],
-      },
-    ];
+// const contacts = [
+//       {
+//         firstName: "Akira",
+//         lastName: "Laine",
+//         number: "0543236543",
+//         likes: ["Pizza", "Coding", "Brownie Points"],
+//       },
+//       {
+//         firstName: "Harry",
+//         lastName: "Potter",
+//         number: "0994372684",
+//         likes: ["Hogwarts", "Magic", "Hagrid"],
+//       },
+//       {
+//         firstName: "Sherlock",
+//         lastName: "Holmes",
+//         number: "0487345643",
+//         likes: ["Intriguing Cases", "Violin"],
+//       },
+//       {
+//         firstName: "Kristian",
+//         lastName: "Vos",
+//         number: "unknown",
+//         likes: ["JavaScript", "Gaming", "Foxes"],
+//       },
+//     ];
 
-    function lookUpProfile(name, prop) {
-      const contact = contacts.find((c) => c.firstName == name);
+//     function lookUpProfile(name, prop) {
+//       const contact = contacts.find((c) => c.firstName == name);
 
-      if  (!contact) return "No such contact!"
+//       if  (!contact) return "No such contact!"
 
-      if (!contact[prop]) return "No such property!"
+//       if (!contact[prop]) return "No such property!"
 
-      return contact[prop];
-    }
+//       return contact[prop];
+//     }
 
-    console.log(lookUpProfile("JavaSript", "Gaming"));
+//     console.log(lookUpProfile("JavaSript", "Gaming"));
+
+// =============================================================
+// =============================================================
+
+const rectangle = { 
+  name: 'Rectangle 1',
+  width: 20,
+  heigth: 10,
+  area: function () {
+    return this.width * this.heigth;
+  },
+};
+
+console.log(this);
