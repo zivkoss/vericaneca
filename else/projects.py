45e2f6551301e20022acab93d32e30094d40e352
Nemanja Zivanovic <zivkoss@yahoo.com>
2024-08-10 10:55:18 +0200

    add new code

--- else/projects.py
+++ else/projects.py
@@ -149,6 +149,10 @@ em['To'] = email_receiver
 em['subject'] = subject
 em.set_content(body)
 
-# //
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(email_sender, email_pasword)
+    smtp.sendmail
 
 
