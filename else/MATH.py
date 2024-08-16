7ae49ec9df868242ea880b961ca52f357e501559
Nemanja Zivanovic <zivkoss@yahoo.com>
2024-08-16 21:11:17 +0200

    add new code

--- else/MATH.py
+++ else/MATH.py
@@ -4,7 +4,7 @@ name = input("What is your name?\n")
 
 print("Hello " + name + ", thank you so much for comming in today.\n\n\n")
 
-menu = "Balck Coffee, Espresso, Latte, Capuccino"
+menu = "Black Coffee, Espresso, Latte, Capuccino"
 
 print(name + ", what would you like from our menu today? Here is what we are serving.\n" + menu)
 
