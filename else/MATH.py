135be0b9fb643f8aebb067968531e951a4b65ad8
Nemanja Zivanovic <zivkoss@yahoo.com>
2024-08-16 08:03:09 +0200

    add new code

--- else/MATH.py
+++ else/MATH.py
@@ -7,3 +7,7 @@ print("Hello " + name + ", thank you so much for comming in today.\n\n\n")
 print = "Balck Coffee, Espresso, Latte, Capuccion"
 
 print(name + ", what would you like from our menu today? Here is what we are serving.\n ")
+
+order = input()
+
+print("Sounds good " + name + ", we'll have that " + order + " ready for you in a moment.")
