bd2212f66e91b083f93dd48ab3d01eab5ac886f1
Nemanja Zivanovic <zivkoss@yahoo.com>
2024-08-17 09:09:11 +0200

    add new code

---
+++ else/codecademy.practise.js
@@ -0,0 +1,16 @@
+//my current age
+const myAge = 25;
+//early ears 
+let earlyYears = 2;
+earlyYears *= 10.5;
+let laterYears = myAge - 2;
+laterYears *= 4;
+
+console.log(earlyYears);
+console.log(laterYears);
+
+let myAgeInDogYeras = earlyYears + laterYears;
+
+let myName = "Brian".toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAgeInDogYeras} years old in dog years.`);
