8f2c1f0f831846098db02556125df868d6c8eb84
Nemanja Zivanovic <zivkoss@yahoo.com>
2024-08-17 14:37:21 +0200

    add new code

--- else/ifElse.py
+++ else/ifElse.py
@@ -1,1 +1,8 @@
 print("Hello, welcome to NetworkChuck Coffee!!!!!!!!!!!!")
+
+name = input("What is your name?\n")
+
+if name == "Ben":
+    print("You're not welcome!!!")
+
+print("Hello " + name + ", thank you so much for coming today.\n\n\n")
